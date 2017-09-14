2bd8c215db8a46423483b08fcf62479c14556698
aadhikar <aadhikar@nyit.edu>
2017-09-14 18:32:24 -0500

    angularPractice
    angular practice

--- theShop/src/app/recipes/recipe-list/recipe-list.component.ts
+++ theShop/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,20 +7,24 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simple test Recipe',
+    new Recipe('First Recipe', 'This is simple test Recipe',
     'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
-    new Recipe('A Test Recipe', 'This is simple test Recipe',
+    new Recipe('Second Recipe', 'This is simple test Recipe',
     'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
-    new Recipe('A Test Recipe', 'This is simple test Recipe',
+    new Recipe('Third Recipe', 'This is simple test Recipe',
     'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
-    new Recipe('A Test Recipe', 'This is simple test Recipe',
+    new Recipe('Fourth Recipe', 'This is simple test Recipe',
     'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
